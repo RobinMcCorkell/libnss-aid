e8bf8f87d0360f6aff2142a1aee00b1c8fc4bdd1
Robin McCorkell <rmccorkell@karoshi.org.uk>
2013-09-12 22:00:32 +0100

    Use LGPG v3 for all files

--- PersistentLooper.hpp
+++ PersistentLooper.hpp
@@ -1,9 +1,21 @@
 // PersistentLooper.hpp
-// part of libnss-aid
-//
 // PersistentLooper header file
+
+// Copyright (C) 2013 Robin McCorkell
+// This file is part of libnss-aid.
 //
-// Copyright (C) 2013 Robin McCorkell <rmccorkell@karoshi.org.uk>
+// libnss-aid is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// libnss-aid is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with libnss-aid.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef HPP_PERSISTENTLOOPER
 #define HPP_PERSISTENTLOOPER

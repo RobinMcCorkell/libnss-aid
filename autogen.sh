151d6f802b4f1d17bfa879cda20ecbfca5a900bf
Robin McCorkell <rmccorkell@karoshi.org.uk>
2013-10-13 16:13:07 +0100

    Fix file name error

--- autogen.sh
+++ autogen.sh
@@ -5,7 +5,7 @@ if [[ -d .git ]] && which git > /dev/null; then
 	[[ -e ChangeLog ]] && rm -f ChangeLog
 	git log --pretty=medium --abbrev-commit > ChangeLog
 else
-	touch Changelog #suppress errors with a missing ChangeLog
+	touch ChangeLog #suppress errors with a missing ChangeLog
 fi
 
 # Prepare the source files for packaging
